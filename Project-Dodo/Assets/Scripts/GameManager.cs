b7e40378e277dbbd73179424789c62a1630fd6f1
Lars Güttinger <Eriklars@gmx.de>
2015-11-22 18:54:51 +0100

    Laptop to PC

---
+++ Project-Dodo/Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : Manager<GameManager>
+{
+    int level = PlayerPrefs.GetInt("Level");
+
+    public void Awake()
+    {
+        if (PlayerPrefs.GetInt("Level")==0)
+        {
+            PlayerPrefs.SetInt("Level",1);
+        }
+    }
+}
+
+
+
---
+++ Project-Dodo/Assets/Scripts/MainCanvas.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+enum Button
+{
+    Exit,
+    Next,
+    Continue,
+    Default
+}
+
+
+public class MainCanvas : MonoBehaviour
+{
+    private Button buttons = new Button();
+    GameManager gameManager;
+
+    public void Awake()
+    {
+        gameManager = FindObjectOfType<GameManager>();
+    }
+
+    public void GetButton(string _buttonName)
+    {
+        try
+        {
+            buttons = (Button)System.Enum.Parse(typeof(Button), _buttonName, true);
+        }
+        catch (System.Exception)
+        {
+            buttons = Button.Default;
+        }
+
+        switch (buttons)
+        {
+            case Button.Exit:
+                if (Application.loadedLevel != 1)
+                {
+                    Application.LoadLevel(1);
+                }
+                else
+                {
+                    Debug.Log("Ich habe mich geschlossen");
+                    Application.Quit();
+                }
+                break;
+            case Button.Next:
+                break;
+            case Button.Continue:
+                if (Application.loadedLevel == PlayerPrefs.GetInt("Level"))
+                {
+                    gameObject.SetActive(false);
+                }
+                else
+                {
+                    Application.LoadLevel(PlayerPrefs.GetInt("Level"));
+                }
+                break;
+            case Button.Default:
+            default:
+                Debug.LogWarning(_buttonName + " not implemented");
+                break;
+        }
+    }
+
+}
+
+
